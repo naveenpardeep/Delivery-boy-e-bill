db1388b3b63916196d01d92c992cc23b2aee352e
Naveen <42085829+naveenpardeep@users.noreply.github.com>
2021-03-29 19:31:26 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,14 @@
+package com.example.transporte_bill;
+
+public class User {
+  public String name,email,phone;
+    public User(){
+
+    }
+    public User(String name,String email,String phone){
+         this.name=name;
+         this.email=email;
+         this.phone=phone;
+    }
+
+}
