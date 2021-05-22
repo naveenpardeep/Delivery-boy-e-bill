b6722c89f4af1af482f4615f8f0ad242444730d8
Naveen <42085829+naveenpardeep@users.noreply.github.com>
2021-05-22 23:07:54 +0530

    Add files via upload

---
+++ app/src/Java/Delivery.java
@@ -0,0 +1,13 @@
+package com.example.transporte_bill;
+
+public class Delivery {
+    public String contact,deliveredto;
+    public Delivery(){
+
+    }
+    public Delivery(String contact,String deliveredto){
+        this.contact=contact;
+        this.deliveredto=deliveredto;
+
+    }
+}
